2424e07b93dcf024ad642a99f727df0ec4c0c1c0
sravan1946 <miniclip1956@gmail.com>
2024-02-17 15:44:39 +0530

    add logout

--- app.py
+++ app.py
@@ -92,3 +92,9 @@ def user_register():
         login_user(user)
         return redirect(url_for("index"))
     return render_template("userregister.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
